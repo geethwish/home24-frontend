6a1634481cb984356d5c2acdf045a693e6f703d8
Geeth Wishkamal <41125548+geethwish@users.noreply.github.com>
2025-03-29 12:48:28 +0530

    basic routes and pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { lazy } from 'react'
+
+const LoginPage = lazy(() => import('./pages/Login'))
+const DashboardPage = lazy(() => import('./pages/Dashboard'))
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
